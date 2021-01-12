dd61b79d377848886e8848344e0aeb5d77bd3abb
281677160 <66651997+281677160@users.noreply.github.com>
2021-01-13 00:29:06 +0800

    Update AutoUpdate1.sh

--- build/AutoUpdate/AutoUpdate1.sh
+++ build/AutoUpdate/AutoUpdate1.sh
@@ -4,7 +4,7 @@
 # AutoBuild Actions
 
 GET_TARGET_INFO() {
-Author=281677160
+Author=danshui
 AutoUpdate_Version=$(awk 'NR==6' package/base-files/files/bin/AutoUpdate.sh | awk -F '[="]+' '/Version/{print $2}')
 TARGET_PROFILE=$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/.*DEVICE_(.*)=y/\1/')
 TARGET_BOARD=$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' .config)
